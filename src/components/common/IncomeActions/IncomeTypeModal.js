56b95a7a051242e0fea034a548527a8b3a62501a
Jessica Ferrari <jscferrari@gmail.com>
2025-07-23 11:47:22 -0300

    chore(income-type): Use redux-form on Income Type Modal

--- src/components/common/IncomeActions/IncomeTypeModal.js
+++ src/components/common/IncomeActions/IncomeTypeModal.js
@@ -16,6 +16,9 @@ import {
 import { Input, SIZE as InputSize } from "baseui/input";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import { FormControl } from "baseui/form-control";
+import { change, reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { toaster, ToasterContainer } from "baseui/toast";
 
 import { useIncomeTypePost } from "../../../hooks/incomeTypes/useIncomeTypePost";
 import { useIncomeTypePut } from "../../../hooks/incomeTypes/useIncomeTypePut";
@@ -28,95 +31,103 @@ const gridOverrides = {
 const checkboxItemOverrides = {
     display: 'flex',
     alignSelf: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: '17px',
 };
 
-const IncomeTypeModal = ({
+const toasterOverrides = {
+    ToastBody: {
+        style: {
+            width: '500px'
+        }
+    }
+};
+
+let IncomeTypeModal = ({
     isOpen,
     onClose,
     reload,
-    incomeType = null
+    incomeType = null,
+    handleSubmit,
+    formErrors,
+    dispatch
 }) => {
     const [isLoading, setIsLoading] = React.useState(false);
-    const [typeName, setTypeName] = React.useState(!!incomeType ? incomeType["name"] : "");
     const [recurrent, setRecurrent] = React.useState(!!incomeType ? incomeType["recurrent"] : false);
-    const [baseValue, setBaseValue] = React.useState(!!incomeType ? incomeType["baseValue"] : 0.00);
-
-    const [formErrors, setFormErrors] = React.useState({});
 
     const { mutateAsync: addIncomeTypeRequest } = useIncomeTypePost();
     const { mutateAsync: editIncomeTypeRequest } = useIncomeTypePut();
 
-    const onSaveClick = async (typeName, recurrent, baseValue = 0) => {
+    const onSubmit = async (values) => {
         setIsLoading(true);
 
         const payload = {
-            'name': typeName,
-            'recurrent': recurrent,
-            'baseValue': baseValue
+            'name': values.name,
+            'recurrent': values.recurrent,
+            'baseValue': values.baseValue
         }
 
-        if (incomeType !== null) {
-            await editIncomeTypeRequest({
-                id: incomeType["incomeTypeId"], 
-                payload: payload
-            });
+        try {
+            if (!!incomeType) {
+                await editIncomeTypeRequest({
+                    id: incomeType["incomeTypeId"], 
+                    payload: payload
+                });
+            }
+            else {
+                await addIncomeTypeRequest(payload);
+            }
+            toaster.positive("Income type saved successfully");
+            await reload();
+            setIsLoading(false);
+            onClose();
+        } catch (error) {
+            toaster.negative(`${error}`);
+            setIsLoading(false);
         }
-        else {
-            await addIncomeTypeRequest(payload);
-        }
-        await reload();
-        setIsLoading(false);
     };
 
-    const clearFields = () => {
-        setFormErrors({});
-        setTypeName(!!incomeType ? incomeType["name"] : "");
-        setRecurrent(!!incomeType ? incomeType["recurrent"] : false);
-        setBaseValue(!!incomeType ? incomeType["baseValue"] : 0.00);
+    const handleRecurrentChange = (value, input) => {
+        input.onChange(value);
+        setRecurrent(!recurrent);
     };
+
+    const renderField = ({ input, label, type, meta: { touched, error } }) => (
+        <FormControl label={label} error={touched && error && error}>
+            <Input
+              {...input}
+              type={type}
+              size={InputSize.compact}
+            />
+        </FormControl>
+    );
+
+    const renderCheckboxField = ({ input, label, meta: { touched, error } }) => (
+        <Checkbox
+            checked={input.value}
+            onChange={(value) => handleRecurrentChange(value, input)}
+            size={InputSize.compact}
+            labelPlacement={LABEL_PLACEMENT.left}
+            {...checkboxItemOverrides}
+        >
+            {label}
+        </Checkbox>
+    );
 
     React.useEffect(() => {
-        if (incomeType !== null) {
-            setTypeName(incomeType["name"]);
+        if (!!incomeType) {
+            dispatch(change('incomeTypeForm', 'name', incomeType["name"]));
+            dispatch(change('incomeTypeForm', 'recurrent', incomeType["recurrent"]));
+            dispatch(change('incomeTypeForm', 'baseValue', incomeType["baseValue"]));
             setRecurrent(incomeType["recurrent"]);
-            setBaseValue(incomeType["baseValue"]);
         }
-        else {
-            clearFields();
-        }
-    }, [incomeType]);
-
-    const handleClose = () => {
-        clearFields();
-        onClose();
-    };
-
-    const validateAndSave = () => {
-        const requiredMessage = "This field is required"
-        let errors = {}
-
-        if (typeName === ""){
-            errors["name"] = requiredMessage;
-        }
-        if (recurrent === true && baseValue <= 0) {
-            errors["baseValue"] = "Please specify a base value"
-        }
-        
-        if (Object.keys(errors).length > 0) {
-            setFormErrors(errors);
-            return;
-        }
-        
-        onSaveClick(typeName, recurrent, baseValue);
-        handleClose();
-
-    };
+    }, [incomeType, dispatch]);
 
     return (
+        <>
+        <ToasterContainer autoHideDuration={10000} overrides={toasterOverrides} />
         <Modal
-        onClose={handleClose}
+        onClose={onClose}
         closeable
         isOpen={isOpen}
         animate
@@ -125,58 +136,70 @@ const IncomeTypeModal = ({
         role={ROLE.dialog}
         >
             <ModalHeader>{!!incomeType ? "Edit" : "Add"} Income Type</ModalHeader>
-            <ModalBody>
-                <FlexGrid flexGridColumnCount={2} flexGridColumnGap={"5px"} {...gridOverrides}>
-                    <FlexGridItem>
-                        <FormControl 
-                            label="Income Type Name *"
-                            error={"name" in formErrors ? formErrors["name"] : null}
-                        >
-                            <Input
-                                clearable
-                                value={typeName}
-                                onChange={e => setTypeName(e.target.value)}
-                                placeholder="Enter Name..."
-                                size={InputSize.compact}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <ModalBody>     
+                    <FlexGrid {...gridOverrides}>
+                        <FlexGridItem>
+                            <Field 
+                                name="name"
+                                type="text"
+                                component={renderField}
+                                label="Income Type Name *"
                             />
-                        </FormControl>
-                    </FlexGridItem>
-                    <FlexGridItem {...checkboxItemOverrides}>
-                        <Checkbox
-                            checked={recurrent}
-                            onChange={e => setRecurrent(e.target.checked)}
-                            labelPlacement={LABEL_PLACEMENT.right}
-                            >
-                            Recurrent
-                        </Checkbox>
-                    </FlexGridItem>
-                </FlexGrid>
-                {recurrent && 
-                    <FormControl 
-                        label="Base Value *"
-                        error={"baseValue" in formErrors ? formErrors["baseValue"] : null}
-                    >
-                        <Input
-                            startEnhancer="R$"
-                            value={baseValue}
-                            onChange={e => setBaseValue(e.target.value)}
-                            placeholder="Enter Base Value..."
-                            size={InputSize.compact}
-                            type="number"
-                        />
-                    </FormControl>
-                }
-            </ModalBody>
-            <ModalFooter>
-                <ModalButton kind={ButtonKind.tertiary} onClick={handleClose}>
-                    Cancel
-                </ModalButton>
-                <ModalButton type={'submit'} onClick={() => validateAndSave()} isLoading={isLoading}>
-                    Save
+                        </FlexGridItem>
+                    </FlexGrid>
+                    <FlexGrid flexGridColumnCount={2} flexGridColumnGap={"5px"} {...gridOverrides}>
+                        <FlexGridItem {...checkboxItemOverrides}>
+                            <Field 
+                                name="recurrent"
+                                component={renderCheckboxField}
+                                label="Recurrent"
+                            />
+                        </FlexGridItem>
+                        {recurrent && 
+                            <FlexGridItem>
+                                <Field 
+                                    name="baseValue"
+                                    component={renderField}
+                                    label="Base Value *"
+                                />
+                            </FlexGridItem>
+                        }
+                    </FlexGrid>
+                </ModalBody>
+                <ModalFooter>
+                    <ModalButton kind={ButtonKind.tertiary} onClick={onClose} type={'button'}>
+                        Cancel
+                    </ModalButton>
+                    <ModalButton type={'submit'} isLoading={isLoading}>
+                        Save
                 </ModalButton>
             </ModalFooter>
+            </form>
         </Modal>
+        </>
     );
 }
 
-export default IncomeTypeModal;
+const validate = (values) => {
+    const errors = {};
+    
+    if (!values.name) {
+        errors.name = 'Required';
+    }
+    
+    if (values.recurrent && !values.baseValue) {
+        errors.baseValue = 'Required';
+    }
+    
+    return errors;
+};
+
+IncomeTypeModal = reduxForm({
+    form: 'incomeTypeForm',
+    enableReinitialize: true,
+    touchOnBlur: false,
+    validate
+})(IncomeTypeModal);
+
+export default connect()(IncomeTypeModal);
